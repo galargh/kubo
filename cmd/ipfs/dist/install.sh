126d043e96bf1f012a69d85b37546351b9a8b15d
galargh <piotr.galar@gmail.com>
2022-10-18 14:19:04 +0200

    chore: prepare repo for snap and chocolatey deprecation

--- cmd/ipfs/dist/install.sh
+++ cmd/ipfs/dist/install.sh
@@ -1,39 +1,6 @@
 #!/bin/sh
-#
-# Installation script for ipfs. It tries to move $bin in one of the
-# directories stored in $binpaths.
 
-INSTALL_DIR=$(dirname $0)
-
-bin="$INSTALL_DIR/ipfs"
-binpaths='/usr/local/bin /usr/bin $HOME/.local/bin'
-
-# This variable contains a nonzero length string in case the script fails
-# because of missing write permissions.
-is_write_perm_missing=""
-
-for raw in $binpaths; do
-  # Expand the $HOME variable.
-  binpath=$(eval echo "$raw")
-  mkdir -p "$binpath"
-  if mv "$bin" "$binpath/ipfs" ; then
-    echo "Moved $bin to $binpath"
-    exit 0
-  else
-    if [ -d "$binpath" ] && [ ! -w "$binpath" ]; then
-      is_write_perm_missing=1
-    fi
-  fi
-done
-
-echo "We cannot install $bin in one of the directories $binpaths"
-
-if [ -n "$is_write_perm_missing" ]; then
-  echo "It seems that we do not have the necessary write permissions."
-  echo "Perhaps try running this script as a privileged user:"
-  echo
-  echo "    sudo $0"
-  echo
-fi
+echo "Kubo is not distributed through Chocolatey anymore (https://github.com/ipfs/kubo/issues/9341)." >&2
+echo "Please download Kubo from https://dist.ipfs.tech/#kubo." >&2
 
 exit 1
